17f173c7cba5583bc4433847dcbc2d7c1a36a0b1
Sam <35919701+SamSkjord@users.noreply.github.com>
2024-07-12 16:49:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tpms",
-    version="0.1.1",
+    version="1.0.1",
     description="Tire Pressure Monitoring System Python Library",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
